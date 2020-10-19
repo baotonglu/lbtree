fbed313c4de6749166f87cfd2cd0f332ff1c574c
baotonglu <lubaotong22@gmail.com>
2020-10-18 20:41:55 -0700

    submit current result

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 rm -rf /mnt/pmem0/baotong/lbtree.data
 
-/usr/bin/hog-machine.sh ./lbtree thread 1 mempool 100 nvmpool "no-pool" 200 bulkload 50000000 keygen-8B/dbg-k50k 1.0 lookup 50000000 keygen-8B/dbg-search500
+/usr/bin/hog-machine.sh ./lbtree thread 1 mempool 100 nvmpool "no-pool" 200 bulkload 100000000 keygen-8B/dbg-k50k 0.6 lookup 100000000 keygen-8B/dbg-delete500
