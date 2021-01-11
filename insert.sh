fbf63ee1541c1f3c31c812f8af7c60f3162a4daf
Baotong LU <baotonglu@Baotongs-MacBook-Pro.local>
2021-01-11 15:37:57 +0800

    update the script

--- insert.sh
+++ insert.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-rm -rf /mnt/pmem0/baotong/lbtree.data
-
-/usr/bin/hog-machine.sh numactl --cpunodebind=0 --membind=0 ./lbtree thread 1 mempool 100 nvmpool "no-pool" 200 bulkload 50000000 keygen-8B/dbg-k50k 0.6 insert 50000000 keygen-8B/dbg-insert500
+thread_num=(0 1 2 4 8 16 24 48)
+for j in {1..6}
+do
+    rm -rf /mnt/pmem0/baotong/lbtree.data
+    /usr/bin/hog-machine.sh numactl --cpunodebind=0 --membind=0 ./lbtree thread ${thread_num[$j]} mempool 100 nvmpool "no-pool" 200 bulkload 50000000 keygen-8B/dbg-k50k 0.7 insert 50000000 keygen-8B/dbg-insert500
+done
--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-rm -rf /mnt/pmem0/baotong/lbtree.data
-
-/usr/bin/hog-machine.sh ./lbtree thread 1 mempool 100 nvmpool "no-pool" 200 bulkload 100000000 keygen-8B/dbg-k50k 0.6 lookup 100000000 keygen-8B/dbg-delete500
+thread_num=(0 1 2 4 8 16 24 48)
+for j in {1..6}
+do
+    rm -rf /mnt/pmem0/baotong/lbtree.data
+    /usr/bin/hog-machine.sh ./lbtree thread ${thread_num[$j]} mempool 100 nvmpool "no-pool" 200 bulkload 100000000 keygen-8B/dbg-k50k 0.7 lookup 100000000 keygen-8B/dbg-delete500
+done    
