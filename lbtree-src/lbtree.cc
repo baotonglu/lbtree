b29b5a6d9ae5c31eac4f4cd4b9544e27a2e09991
baotonglu <lubaotong22@gmail.com>
2021-01-29 11:09:29 +0800

    create new benchmark for lbtree

--- lbtree-src/lbtree.cc
+++ lbtree-src/lbtree.cc
@@ -1824,6 +1824,13 @@ tree * initTree(void *nvm_addr, bool recover)
     return mytree;
 }
 
+lbtree * initLBTree(void *nvm_addr, bool recover)
+{   
+    lbtree *mytree = new lbtree(nvm_addr, recover);
+    std::cout << "Successfully create my lbtree" << std::endl;
+    return mytree;
+}
+
 int main (int argc, char *argv[])
 {
     printf("NON_LEAF_KEY_NUM= %d, LEAF_KEY_NUM= %d, nonleaf size= %lu, leaf size= %lu\n",
@@ -1836,9 +1843,52 @@ int main (int argc, char *argv[])
     my_alloc::BasePMPool::Initialize(pool_name, pool_size);
     my_alloc::BasePMPool::IncreaseAllocatorNum(); 
 
-    parse_command (argc, argv);
+    //parse_command (argc, argv);
+    // create the tree
+    char *nvm_addr;
+    my_alloc::BasePMPool::ZAllocate((void **)&nvm_addr, 4*KB);
+    lbtree* index = initLBTree(nvm_addr, false);
+      
+    // first bulk load, then do the insert operation
+    int keynum;
+    key_type* keys;
+    if (strcmp(argv[0], "bulkload") == 0)
+    {
+      keynum = atoi (argv[1]);
+      char* keyfile = argv[2];
+      float bfill; sscanf (argv[3], "%f", &bfill);
 
-    std::cout << "Node Split = " << node_split << std::endl;
+      keys = new key_type[keynum];
+      load_binary_data(keys, keynum, keyfile);
+      std::sort(keys, keys + keynum / 2,
+            [](key_type const& a, key_type const& b) { return a < b; });
+      std::cout << "start the bulkload" << std::endl;
+      int level = index->bulkload (keynum / 2, keys, bfill);
+      std::cout << "finish the bulkload, root is at level " << level << std::endl;
+      key_type start, end;
+      index->check (&start, &end);
+      std::cout << "the index check is good" << std::endl;
+    }
+
+    // start the insert
+    key_type* key = keys + (keynum / 2);
+    int insert_num = keynum / 2;
+    for (int i = 0; i < insert_num; ++i)
+    {
+      key_type kk = key[i];
+      index->insert (kk, (void *) (&key[ii]));
+    }
+    std::cout << "finish the insert" << std::endl;
+    // debug insert
+    int found = 0;
+    for (int ii=0; ii<insert_num; ii++) {
+       void *p;
+       int pos;
+       p = index->lookup (key[ii], &pos);
+       found += (pos >= 0);
+    }
+    std::cout << "Found = " << found << std::endl;
+    //std::cout << "Node Split = " << node_split << std::endl;
     std::cout << "Used PM = " << size_of_PM << std::endl;
     std::cout << "Used DRAM = " << size_of_DRAM << std::endl;
     my_alloc::BasePMPool::get_collected();
